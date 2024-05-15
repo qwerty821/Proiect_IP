ee7623bd54f0e4021d1238aa2c98381fadf62f85
dan-butnaru <dan.butnaru111@gmail.com>
2024-05-15 15:53:28 +0300

    explorer

--- Proiect_IP/Pages/SearchPage.Designer.cs
+++ Proiect_IP/Pages/SearchPage.Designer.cs
@@ -32,14 +32,16 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
             // searchButton
             // 
             this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchButton.Location = new System.Drawing.Point(1218, 73);
+            this.searchButton.Location = new System.Drawing.Point(874, 51);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(108, 60);
+            this.searchButton.Size = new System.Drawing.Size(72, 39);
             this.searchButton.TabIndex = 4;
             this.searchButton.Text = "Cauta";
             this.searchButton.UseVisualStyleBackColor = true;
@@ -48,43 +50,53 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(818, 74);
+            this.textBox1.Location = new System.Drawing.Point(628, 52);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(284, 53);
+            this.textBox1.Size = new System.Drawing.Size(229, 38);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(74, 88);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(91, 54);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(240, 55);
+            this.button1.Size = new System.Drawing.Size(160, 36);
             this.button1.TabIndex = 5;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(395, 78);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Location = new System.Drawing.Point(373, 54);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(254, 60);
+            this.button2.Size = new System.Drawing.Size(160, 36);
             this.button2.TabIndex = 6;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(61, 148);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(885, 470);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
             // SearchPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1478, 944);
+            this.ClientSize = new System.Drawing.Size(985, 614);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.searchButton);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SearchPage";
             this.Text = "SearchPage";
+            this.Load += new System.EventHandler(this.SearchPage_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,5 +107,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- Proiect_IP/Pages/SearchPage.cs
+++ Proiect_IP/Pages/SearchPage.cs
@@ -13,11 +13,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TMDbLib.Client;
 using TMDbLib.Objects.Movies;
+using TMDbLib.Objects.Search;
 using static Proiect_IP.Res;
 namespace Pages
 {
     public partial class SearchPage : Form
     {
+        private Action<object, States> _callBackFunc;
+
         public SearchPage()
         {
             InitializeComponent();
@@ -26,6 +29,7 @@ namespace Pages
         public void SetCallBack(Action<object, States> action)
         {
             searchButton.Click += delegate { action(this, States.Movies_ListState); };
+
         }
 
         private void searchButton_Click(object sender, EventArgs e)
@@ -37,7 +41,6 @@ namespace Pages
 
             var movie = client.SearchMovieAsync(title).Result;
             var movies = movie.Results;
-            //asdasdasdasd
             SaveToJson(movies);
         }
 
@@ -46,6 +49,61 @@ namespace Pages
             string JsonResult = JsonConvert.SerializeObject(movies);
             Res.WriteMovies(JsonResult);
             
+        }
+
+        private void SearchPage_Load(object sender, EventArgs e)
+        {
+            TMDbClient client = new TMDbClient("ba989c6148c4f9e4f7456f4d3ba6a8b7");
+            var mov = client.GetMoviePopularListAsync().Result;
+            var list = mov.Results.ToList();
+
+
+            foreach (SearchMovie movie in list)
+            {
+                FlowLayoutPanel moviePanel = new FlowLayoutPanel();
+                moviePanel.FlowDirection = FlowDirection.TopDown;
+                //moviePanel.AutoSize = true;
+                moviePanel.Size = new Size(360, 600);
+                moviePanel.Padding = new Padding(25);
+                moviePanel.BorderStyle = BorderStyle.FixedSingle;
+
+                moviePanel.Click += delegate { SelectedMovie(movie); };
+
+                moviePanel.Click += delegate {
+                    _callBackFunc(this, States.Movie_InfoState);
+
+                };
+
+                Label title = new Label();
+                title.Text = movie.Title;
+                title.TextAlign = ContentAlignment.MiddleCenter;
+                title.Font = new Font("Microsoft Sans Serif", 20);
+                title.Width = 300;
+                title.Height = 70;
+                title.BorderStyle = BorderStyle.FixedSingle;
+                PictureBox pictureBox = new PictureBox();
+
+                pictureBox.Click += delegate { SelectedMovie(movie); };
+
+                pictureBox.Click += delegate {
+                    _callBackFunc(this, States.Movie_InfoState);
+                };
+
+                pictureBox.Size = new Size(300, 450);
+                pictureBox.ImageLocation = Res.imageUrl + movie.PosterPath;
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+                moviePanel.Controls.Add(pictureBox);
+                moviePanel.Controls.Add(title);
+
+                flowLayoutPanel1.Controls.Add(moviePanel);
+            }
+
+        }
+        private void SelectedMovie(object sender)
+        {
+            Res.SetSelectedMovie(((SearchMovie)sender).Id);
         }
     }
 }
